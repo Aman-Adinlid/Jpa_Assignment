587158a8753d614da937a6cfd130b481313af62f
aman adinlid <95pinkpanda@gmail.com>
2021-03-04 18:27:29 +0100

    ##Created Ingredient class and did unit test for it##

---
+++ src/test/java/se/lexicon/jpa/entity/IngredientTest.java
@@ -0,0 +1,42 @@
+package se.lexicon.jpa.entity;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class IngredientTest {
+
+    Ingredient ingredient;
+
+    @BeforeEach
+    public void setup() {
+        ingredient = new Ingredient(1, "Pork");
+
+    }
+
+    @Test
+    @DisplayName("Test1 create ingredient")
+    public void test1() {
+        Assertions.assertEquals(1, ingredient.getIngredientId());
+        Assertions.assertEquals("Pork", ingredient.getIngredientName());
+    }
+
+    @Test
+    @DisplayName("Test 2: test equal method")
+    public void test2() {
+        Ingredient expected = new Ingredient(1, "Pork");
+        Ingredient actual = ingredient;
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    @DisplayName("Test 3: test hashcode Ingredient")
+    public void test_hashCode() {
+        Ingredient expected = new Ingredient(1, "Pork");
+        Ingredient actual = ingredient;
+        Assertions.assertEquals(expected.hashCode(), actual.hashCode());
+    }
+}
